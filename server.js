346b8e942bf3c375676e868c4ca3d183d6407c18
Ali <97852605+AliBee87@users.noreply.github.com>
2022-04-28 15:22:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
-app.use('/update', require('./routes/update));
+app.use('/update', require('./routes/update'));
 
 app.use(verifyJWT);
 app.use('/recipes', require('./routes/api/recipes'));
